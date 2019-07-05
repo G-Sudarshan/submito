18af47b9f7c16b3ccf867aaa7ed36f0ce664ec96
Sudarshan gawale <11sudarshang@gmail.com>
2019-07-05 08:47:52 +0530

    modified:   application/views/Admin/Admin_dashboard.php

--- application/views/Admin/Admin_dashboard.php
+++ application/views/Admin/Admin_dashboard.php
@@ -2,10 +2,50 @@
 <html>
 <head>
 	<title>Admin Panel</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<style>
+#myDIV {
+  width: 100%;
+  padding: 50px 0;
+
+ 
+  margin-top: 20px;
+  display: none;
+}
+</style>
+
+	<script type="text/javascript" language="javascript">
+
+		function edit_clg_name()
+		{
+            var x = document.getElementById("myDIV");
+            if (x.style.display === "none") {
+               x.style.display = "block";
+                 } else {
+                    x.style.display = "none";
+               }
+		}
+		
+	</script>
+
 </head>
 <body>
 
 	Admin dashnoard
+
+<button type="button" class="btn btn-outline-primary" onclick="edit_clg_name()" >Edit college name</button>
+<button type="button" class="btn btn-outline-secondary">Secondary</button>
+<button type="button" class="btn btn-outline-success">Success</button>
+<button type="button" class="btn btn-outline-info">Info</button>
+<button type="button" class="btn btn-outline-warning">Warning</button>
+<button type="button" class="btn btn-outline-danger">Danger</button>
+
+<br/>
+<br/>
+
+<div id="myDIV">
+<input type="text" name="clg_name">
+</div>
 
 </body>
 </html>
