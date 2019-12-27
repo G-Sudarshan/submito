411a9fadb0bd68bc40991495a02189b8e36fcf42
G Sudarshan <11gsudarshan@gmail.com>
2019-12-27 21:02:31 +0530

    SQL FILE UPDATED

--- cms_27_dec_2019.sql
+++ cms_27_dec_2019.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 17, 2019 at 04:58 PM
+-- Generation Time: Dec 27, 2019 at 04:31 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.1.33
 
@@ -48,8 +48,8 @@ CREATE TABLE `assignments` (
 --
 
 INSERT INTO `assignments` (`id`, `pdf_path`, `course_code`, `rollno`, `assignment_no`, `upload_datetime`, `marks`, `checked`, `checked_by`, `checked_on`, `student_id`, `text`) VALUES
-(35, 'assets/a/176101_CM6218_1_.pdf', 'CM6218', 176101, 1, '2019-12-16 03:39:42', 0, 0, '', '0000-00-00 00:00:00', 13, ''),
-(36, 'assets/a/176101_CM6218_2_.pdf', 'CM6218', 176101, 2, '2019-12-17 09:33:37', 0, 0, '', '0000-00-00 00:00:00', 13, '');
+(35, 'assets/a/176101_CM6218_1_1.pdf', 'CM6218', 176101, 1, '2019-12-16 03:39:42', 19, 1, '7', '2019-12-18 01:48:41', 13, ''),
+(36, 'assets/a/176101_CM6218_2_1.pdf', 'CM6218', 176101, 2, '2019-12-17 09:33:37', 0, 0, '', '0000-00-00 00:00:00', 13, '');
 
 -- --------------------------------------------------------
 
@@ -69,7 +69,8 @@ CREATE TABLE `events` (
 --
 
 INSERT INTO `events` (`id`, `title`, `start_date`, `end_date`) VALUES
-(13, 'The new event', '2019-12-16', '2019-12-17');
+(13, 'The new event', '2019-12-16', '2019-12-17'),
+(14, 'UDAAN', '2019-12-18', '2019-12-19');
 
 -- --------------------------------------------------------
 
@@ -82,17 +83,16 @@ CREATE TABLE `notes` (
   `path` text NOT NULL,
   `course_code` varchar(20) NOT NULL,
   `teacher_name` varchar(50) NOT NULL,
-  `teacher_id` int(11) NOT NULL
+  `teacher_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `notes`
 --
 
-INSERT INTO `notes` (`id`, `path`, `course_code`, `teacher_name`, `teacher_id`) VALUES
-(1, 'assets/notes/hi.pdf', 'CM6218', 'Mali Sir', 7),
-(2, 'assets/notes/a.jpg', 'CM6218', 'Mali Sir', 7),
-(3, 'assets/notes/JQE-Template.doc', 'CM6237', 'Khedkar Sir', 8);
+INSERT INTO `notes` (`id`, `path`, `course_code`, `teacher_name`, `teacher_id`, `title`) VALUES
+(4, 'assets/notes/submito_paper.pdf', 'CM6218', 'Mali Sir', 7, 'Paper on Submito');
 
 -- --------------------------------------------------------
 
@@ -182,7 +182,15 @@ CREATE TABLE `tbl_courses_db` (
 INSERT INTO `tbl_courses_db` (`id`, `course_code`, `name`, `department_id`) VALUES
 (23, 'CM6218', 'Programming in C', 61),
 (24, 'CM6237', 'Object Oriented Programming', 61),
-(26, 'IF5118', 'Programming in C', 51);
+(26, 'IF5118', 'Programming in C', 51),
+(27, 'CM6230', 'Java Programming', 61),
+(28, 'CM6243', 'Microprocessor', 61),
+(29, 'CM6235', 'Data Structure Using \'C\'', 61),
+(30, 'CM6220', 'professional practices', 61),
+(31, 'CM6240', 'Scripting Technology', 61),
+(32, 'CM6250', 'Software Engineering', 61),
+(33, 'CM6239', 'Web Page Designing', 61),
+(34, 'CM6103', 'Applied Mathematics', 61);
 
 -- --------------------------------------------------------
 
@@ -204,7 +212,8 @@ INSERT INTO `tbl_departments` (`id`, `dpt_id`, `dpt_name`) VALUES
 (12, 11, 'Mechanical'),
 (13, 21, 'Civil'),
 (14, 51, 'Information Technology'),
-(15, 61, 'Computer Technology');
+(15, 61, 'Computer Technology'),
+(16, 144, 'tech');
 
 -- --------------------------------------------------------
 
@@ -223,7 +232,7 @@ CREATE TABLE `tbl_info` (
 --
 
 INSERT INTO `tbl_info` (`id`, `title`, `value`) VALUES
-(1, 'college_name', 'Government Polytechnic Nashik'),
+(1, 'college_name', 'a'),
 (2, 'reset_by', '7');
 
 -- --------------------------------------------------------
@@ -249,11 +258,12 @@ CREATE TABLE `tbl_student_db` (
 --
 
 INSERT INTO `tbl_student_db` (`id`, `rollno`, `name`, `department`, `department_id`, `mobile_no`, `email`, `year`, `password`) VALUES
-(13, 176101, '', 'Computer Technology', 61, 0, '', '', 0x3961323666653766323030643437396437643137356636626163653035366263),
+(13, 176101, 'Suraj Ahire', 'Computer Technology', 61, 7056649996, '11sudarshang@gmail.com', 'Third Year', 0x3961323666653766323030643437396437643137356636626163653035366263),
 (14, 176103, '', 'Computer Technology', 61, 0, '', '', 0x3563353830613361313533393938373035393930373364646132326139646137),
 (18, 175101, '', 'Information Technology', 51, 0, '', '', 0x3635343937306664616563346463393263666132326533333766333062656233),
 (19, 176106, '', 'Computer Technology', 61, 0, 'gaurav@gmail.com', '', 0x6165633165333363393734333737666439653464653861393462386263633338),
-(21, 176110, '', 'Computer Technology', 61, 0, '', '', 0x6231663134363238383639313862303933343662653937626337663532333638);
+(21, 176110, '', 'Computer Technology', 61, 0, '', '', 0x6231663134363238383639313862303933343662653937626337663532333638),
+(22, 1010, '', 'Mechanical', 11, 0, '', '', 0x6435386461383232383939333964386334656334663430363839633238343765);
 
 -- --------------------------------------------------------
 
@@ -279,7 +289,9 @@ CREATE TABLE `tbl_teachers_db` (
 
 INSERT INTO `tbl_teachers_db` (`id`, `staff_id`, `name`, `department`, `department_id`, `username`, `password`, `email`, `mobile_no`) VALUES
 (7, 1, 'Mali Sir', 'Computer Technology', 61, 'malisir@gpnashik.org', 0x6630643336656530343363313165333134373563356262383464623531646433, '11starktony@gmail.com', 0),
-(8, 2, 'Khedkar Sir', 'Computer Technology', 61, 'khedkarsir@gpnashik.org', 0x3263306632316562313862653362306561376134626666623535343331376166, '', 0);
+(8, 2, 'Khedkar Sir', 'Computer Technology', 61, 'khedkarsir@gpnashik.org', 0x3263306632316562313862653362306561376134626666623535343331376166, '', 0),
+(9, 15222, 'aaaaaa', 'tech', 144, 'awaw', 0x3162626438383634363038323730313565356436303565643434323532323531, '', 0),
+(10, 324, 'Sanap Sir', 'Computer Technology', 61, 'sanapsir@gmail.com', 0x3763653762613534373433643238616336336436396630373066363134356234, '', 0);
 
 --
 -- Indexes for dumped tables
@@ -370,13 +382,13 @@ ALTER TABLE `assignments`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `notes`
 --
 ALTER TABLE `notes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `notification`
@@ -400,13 +412,13 @@ ALTER TABLE `tbl_admin_login`
 -- AUTO_INCREMENT for table `tbl_courses_db`
 --
 ALTER TABLE `tbl_courses_db`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '1', AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '1', AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `tbl_departments`
 --
 ALTER TABLE `tbl_departments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '1', AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '1', AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `tbl_info`
@@ -418,13 +430,13 @@ ALTER TABLE `tbl_info`
 -- AUTO_INCREMENT for table `tbl_student_db`
 --
 ALTER TABLE `tbl_student_db`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '1', AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '1', AUTO_INCREMENT=24;
 
 --
 -- AUTO_INCREMENT for table `tbl_teachers_db`
 --
 ALTER TABLE `tbl_teachers_db`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '1', AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '1', AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
